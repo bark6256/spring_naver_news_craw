42f856d0d3d7bc8b9a0e91df50a0ab412a310c12
PYS <bark6256@gmail.com>
2021-09-28 15:08:48 +0900

    프로젝트 세팅

---
+++ src/main/java/com/cos/newsbatchcarw/NewsbatchcarwApplication.java
@@ -0,0 +1,13 @@
+package com.cos.newsbatchcarw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsbatchcarwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsbatchcarwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cos/newsbatchcarw/NewsbatchcarwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.newsbatchcarw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsbatchcarwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
